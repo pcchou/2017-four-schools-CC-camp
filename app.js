d5572d761f326a1c158e99331db7f1eadc778161
oToToT <ty1208chiang@Gmail.com>
2017-05-14 11:47:19 +0800

    modify assests and images

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'assets')));
+app.use(express.static(path.join(__dirname, 'images')));
 
 app.use('/', index);
 app.use('/users', users);
